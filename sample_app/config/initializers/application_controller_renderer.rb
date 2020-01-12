6fb5ffa4eba735d1092d5b0b87c3cebedcce43db
core-melt <core.melt21@gmail.com>
2020-01-12 12:11:29 +0900

    [Finish]3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6a895503a3ffe1795bec48e7971e57c69c3ec19696226cc92d201a63af29475259f80bd554abafc2bf00afbfe2f0a3ea8083412240db0f56ea0623a1e6a71fcc'
--- sample_app/db/schema.rb
+++ sample_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_09_083857) do
+ActiveRecord::Schema.define(version: 2020_01_11_103754) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_01_09_083857) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "image_id"
   end
 
 end
